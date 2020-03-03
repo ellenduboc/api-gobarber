3c86ed57cc417e33755b0c9860a69de42e776f99
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2020-03-03 16:23:06 -0300

    Creating model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Word' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1238712387',
+  });
+
+  return res.json(user);
 });
 
 module.exports = routes;
